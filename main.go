80e329518984a6cf3df56637aff08fe32e72f1e7
lucas-de-lima <lucasramosdelima@gmail.com>
2025-07-27 13:36:07 -0300

    feat: write file done

--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 )
 
 
-
+func writeFile(fp string, ct string) (error) {
+	 err := os.WriteFile(fp, []byte(ct), 0666)
+	 return err
+}
 
 func readFilePath(fp string) (string, error) {
 	data, err := os.ReadFile(fp)
@@ -24,14 +27,23 @@ func readFilePath(fp string) (string, error) {
 
 func main() {
 	fmt.Println("CLI Text Editor - Go vanilla!")
+	filePath := "./document.txt"
 
-	fileData, err := readFilePath("./document.txt")
+	// fileData, err := readFilePath(filePath)
+	// if err != nil {
+	// 	fmt.Println("Fail to read file")
+	// }
+	// if fileData == "" {
+	// 	fmt.Println("Empty File!")
+	// }
+
+	// fmt.Println("Archive content: ", fileData)
+	fmt.Println("________________________________")
+
+	newContentToWrite := "Hello, my name is Lucas!!!"
+	err := writeFile(filePath, newContentToWrite)
 	if err != nil {
-		fmt.Println("Fail to read file")
+		fmt.Println("Fail to write new content")
 	}
-	if fileData == "" {
-		fmt.Println("Empty File!")
-	}
-
-	fmt.Println("Archive content: ", fileData)
+	fmt.Println("Success to write new content!")
 }
